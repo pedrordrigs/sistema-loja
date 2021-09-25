141d573e567eb06902232fdd7e7af1aa2406e2ba
Davi Seagull <davi.seagull@gmail.com>
2021-09-25 16:43:58 -0300

    adding ClienteController and other configs

--- backend/src/main/java/com/dseagull/sistemaloja/controller/ClienteController.java
+++ backend/src/main/java/com/dseagull/sistemaloja/controller/ClienteController.java
@@ -1,2 +1,34 @@
-package com.dseagull.sistemaloja.controller;public class ClienteController {
+package com.dseagull.sistemaloja.controller;
+
+import com.dseagull.sistemaloja.entity.Cliente;
+import com.dseagull.sistemaloja.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("cliente/")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<Object> getCliente(@PathVariable("id") Long id) {
+        Cliente cliente = clienteService.findById(id);
+        return (cliente == null) ? ResponseEntity.ok("Cliente não encontrado") : ResponseEntity.ok(cliente);
+    }
+
+    @GetMapping("list")
+    public ResponseEntity<Object> getClientes() {
+        return ResponseEntity.ok(clienteService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveCliente(@RequestParam("nome") String nome, @RequestParam("cpf") Integer cpf) {
+        Cliente cliente = clienteService.save(nome, cpf);
+        return ResponseEntity.ok(cliente);
+    }
+
+
 }
